d5ec818de4636f8f013b12e14356bf2e886eacb0
Kazuma NAGAI <itb4769we@gmail.com>
2021-05-31 15:29:29 +0900

    Nodeではprocess.argv[index]でコマンドライン引数を取得できる

--- abc086_a_product.js
+++ abc086_a_product.js
@@ -1,13 +1,28 @@
-function main(input) {
-  const args = input.split(' ');  // String.prototype.split()は指定した区切り文字で引数を配列に分割する関数。
-  const a = parseInt(args[0], 10);  // parseInt()は文字列を整数に変換するグローバル関数。第2引数（基数, 底）は省略してもだいたい10になる
-  const b = parseInt(args[1], 10);
+// function main(input) {
+//   const args = input.split(' ');  // String.prototype.split()は指定した区切り文字で引数を配列に分割する関数。
+//   const a = parseInt(args[0], 10);  // parseInt()は文字列を整数に変換するグローバル関数。第2引数（基数, 底）は省略してもだいたい10になる
+//   const b = parseInt(args[1], 10);
 
-  if ((a * b) % 2 === 0) {
-      console.log('\nEven');
+//   if ((a * b) % 2 === 0) {
+//       console.log('\nEven');
+//   } else {
+//       console.log('\nOdd');
+//   }
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));  // /dev/stdinは入力装置をファイルとして操作できるようにしたもの。
+
+
+
+const main2 = () => {
+  const a = process.argv[2];  // Nodeではコマンドライン引数をprocess.argv[index]で取得することができる。
+  const b = process.argv[3];
+
+  if((a * b) % 2 === 0) {
+    console.log('even');
   } else {
-      console.log('\nOdd');
+    console.log('odd');
   }
 }
 
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));  // /dev/stdinは入力装置をファイルとして操作できるようにしたもの。
+main2();
