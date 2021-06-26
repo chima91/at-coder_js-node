67187046154b2dd52fd62a3936f46cac5711ffae
Kazuma NAGAI <itb4769we@gmail.com>
2021-06-26 11:15:31 +0900

    String.prototype.replace()メソッドは、第一引数に一致する文字列を第二引数で置き換えた新しい文字列を返す

---
+++ abc085_a_already-2018.js
@@ -0,0 +1,8 @@
+// https://atcoder.jp/contests/abc085/tasks/abc085_a
+
+const main = stdin => {
+  let S = stdin;
+  console.log('\n' + S.replace('2017', '2018'));
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
