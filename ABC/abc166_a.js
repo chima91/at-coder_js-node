4702d5ef1368d831e509fe570fa6500b12d41d51
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-18 11:20:27 +0900

    標準入力で受け取った文字列には、見えないスペースや改行が含まれている場合もあるらしいため、trim()が必要。

---
+++ ABC/abc166_a.js
@@ -0,0 +1,8 @@
+// https://atcoder.jp/contests/abc166/tasks/abc166_a
+
+const S = require('fs').readFileSync('/dev/stdin', 'utf-8').trim();
+if(S === 'ABC') {
+  console.log('ARC');
+} else {
+  console.log('ABC');
+}
