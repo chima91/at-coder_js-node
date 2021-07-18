06e9570b36f97a632edd1d4c715d27cdf76c215b
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-18 14:45:42 +0900

    長針と短針が1分と1時間でそれぞれ何度動くのかを明確にする。

--- ABC/abc030_b_tokeiban.js
+++ ABC/abc030_b_tokeiban.js
@@ -4,23 +4,19 @@ const main = input => {
   let [ n, m ] = input.trim().split(' ').map(input => parseFloat(input));
   if(n > 12) n -= 12;
 
-  const nOneMinute = 360/60;
   const nOneHour = 360/12;
-  const mOneHour = 360/12;
-  const mOneMinute = 360/12/60;
+  const mOneMinute = 360/60;
 
-  const nAngle = (n * nOneHour) + (m * nOneMinute);
-  const mAngle = (n * mOneHour) + (m * mOneMinute);
+  const nAngle = (n * nOneHour) + (m/60 * nOneHour);
+  const mAngle = m * mOneMinute;
 
-  console.log(n, m, nAngle, mAngle);
+  const angleSubtraction = Math.abs(nAngle - mAngle);
 
-  // const angleSubtraction = Math.abs(nAngle - mAngle);
-
-  // if(angleSubtraction > 180) {
-  //   console.log(360 - angleSubtraction);
-  // } else {
-  //   console.log(angleSubtraction);
-  // }
+  if(angleSubtraction > 180) {
+    console.log(360 - angleSubtraction);
+  } else {
+    console.log(angleSubtraction);
+  }
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf-8'));
