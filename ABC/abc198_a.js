939d4ed8f7d7c0a2fa47f8866e27f2323f132f47
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-19 13:31:31 +0900

    A君とB君がもらうお菓子の個数の和はNになるため、片方が確定すればもう片方も確定する。

---
+++ ABC/abc198_a.js
@@ -0,0 +1,9 @@
+// https://atcoder.jp/contests/abc198/tasks/abc198_a
+
+const main = input => {
+  const N = parseInt(input.trim());
+
+  console.log(N - 1);
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf-8'));
