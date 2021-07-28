a5e55d373963875834b7cba55ac39a65a2990a98
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-28 23:07:20 +0900

    場合分けして、食べれるCの枚数と解毒剤クッキーの和+1を加算したものを出力する。

---
+++ AGC/agc030_a.js
@@ -0,0 +1,9 @@
+// https://atcoder.jp/contests/agc030/tasks/agc030_a
+
+const main = input => {
+  const [A, B, C] = input.trim().split(' ').map(x => parseInt(x));
+
+  (A + B + 1 >= C) ? console.log(B + C) : console.log(B + (A + B + 1));
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf-8'));
