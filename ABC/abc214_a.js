28d92d402817d6f13d80714e772458791063cdf1
Kazuma NAGAI <itb4769we@gmail.com>
2021-08-15 20:17:09 +0900

    正しいコードに修正

--- ABC/abc214_a.js
+++ ABC/abc214_a.js
@@ -1,7 +1,15 @@
-const main = input => {
-  const [A, B, C] = input.trim().split(' ').map(x => parseInt(x));
+// https://atcoder.jp/contests/abc214/tasks/abc214_a
 
-  (A + B + 1 >= C) ? console.log(B + C) : console.log(B + (A + B + 1));
+const main = input => {
+  const N = parseInt(input, 10);
+
+  if(N >= 1 && N <= 125) {
+    console.log(4);
+  } else if(N >= 126 && N <= 211) {
+    console.log(6);
+  } else {
+    console.log(8);
+  }
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf-8'));
